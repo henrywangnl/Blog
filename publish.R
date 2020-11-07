cb6990305419ae98d5552da8e87c758eb167dcf6
Henry <henrywangnl@gmail.com>
2020-11-07 23:50:10 +0100

    new post HubSpot properties

--- publish.R
+++ publish.R
@@ -1,8 +1,25 @@
 
 library(goodpress)
 
-post_folder <- "20201025"
+post_folder <- "20201107"
 wordpress_url <- "https://henrywang.nl"
 
 wp_post(post_folder, wordpress_url)
+
+categories <- wp_categories(wordpress_url)
+
+tags <- wp_tags(wordpress_url)
+View(tags)
+
+
+text <- "HubSpot Contact Properties List"
+gsub(" ", "-", tolower(text))
+
+
+time <- "YYYY-MM-DDTHH:MM:SS"
+format(Sys.time(), '%Y-%m-%dT%H:%M:%S')
+
+toString(text)
+?sub
+
 
